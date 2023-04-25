5fa01f4850488ee10a3fb04f861ba5d61421c328
festusgakure <festusgakure@gmail.com>
2023-04-25 01:15:09 -0700

    Added _specifier file

---
+++ _specifier.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _specifier - checks a character and assigns a function to print
+ * @format: specifies the format
+ *
+ * Return: pointer to function
+ */
+
+int (*_specifier(const char *format))(va_list)
+{
+	int i;
+
+	fn_comp arr[6] = {
+		{"c", p_char},
+		{"s", p_str},
+		{"%", p_cent};
+		{NULL, NULL}};
+
+	for (i = 0; arr[i].comp != NULL; i++)
+	{
+		if (*(arr[i].comp) == *format)
+		{
+			return (arr[i].p);
+		}
+	}
+
+	return (NULL);
+}
