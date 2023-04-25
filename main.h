c8efd4ee3c81067baa7e94afb32737786a771909
festusgakure <festusgakure@gmail.com>
2023-04-25 00:10:29 -0700

    Added main.h file

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+int (*_specifier(const char *))(va_list);
+
+/**
+ * struct fn - structure to specify the printer
+ * @comp: character to be compared
+ * @p: function for  printing
+ */
+typedef struct fn
+{
+	char *comp;
+	int (*p)(va_list);
+} fn_comp;
+
+int p_char(va_list);
+int p_str(va_list);
+int p_cent(va_list);
+int p_int(va_list);
+int p_dec(va_list);
+
+#endif
